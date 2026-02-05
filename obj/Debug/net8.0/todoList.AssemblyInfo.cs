c6add7355c0f3d7513f7c671638d851032c745bd
Betsy Velazquez <betsy.velazquez.osorio@gmail.com>
2026-02-05 01:01:55 -0600

    q

--- obj/Debug/net8.0/todoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/todoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6de9c157aa12399d55429b728f3433977a1315e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf1902c85be13aa2f2b47c2c47ffdbb15b47046")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
